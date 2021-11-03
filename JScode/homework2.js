7aab9ca63e9d740ce56f8a8ad8e3586d3a1860eb
Dmytro <kazimir.atk@gmail.com>
2021-11-03 21:47:51 +0200

    HomeWork2

---
+++ JScode/homework2.js
@@ -0,0 +1,17 @@
+let userName;
+let userAge;
+let userSurname;
+let newUser;
+let shoppingList;
+let userOnline;
+let userSalary;
+let cursorCoordinates;
+
+console.log('Hello world');
+
+userName = 'Kazimir94';
+
+console.log('userName' , userName);
+
+const userAdress = 'UA';
+console.log('useruserAdress' , userAdress);
